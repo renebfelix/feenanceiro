e83dc8f5b520070ebd095fbd5eb46f3af5eaf824
Rene Felix <rennebf@gmail.com>
2024-05-27 01:25:58 -0300

    Init blank templates projects

--- packages/components/Header/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function Header({children}: {children: React.ReactNode}){
-	return <header style={{background: "red", height: "150px"}}>{children}</header>
-}
---
+++ packages/utils/dates.tsx
@@ -0,0 +1,29 @@
+export const fullMonthsName = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+]
+
+export const minMonthsName = [
+    "Jan",
+    "Fev",
+    "Mar",
+    "Abr",
+    "Mai",
+    "Jun",
+    "Jul",
+    "Ago",
+    "Set",
+    "Out",
+    "Nov",
+    "Dez"
+]
