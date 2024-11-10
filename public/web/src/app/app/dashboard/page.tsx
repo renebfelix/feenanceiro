f075966cd3cc22d4b662bbd2df1463a8e60ef5bc
Rene Felix <rennebf@gmail.com>
2024-11-09 23:43:47 -0300

    Dashboard message

--- public/web/src/app/app/dashboard/page.tsx
+++ public/web/src/app/app/dashboard/page.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+import { Box, Flex } from "@chakra-ui/react";
+import { EmptySpace } from "../components/EmptySpace/EmptySpace";
+
 export default function DashboardPage(){
-	return <h1>Dashboard</h1>;
+	return (
+		<Flex borderRadius={8} height={"50vh"} justifyContent={"center"} alignItems={"center"} bg={"white"}>
+			<Box maxW={"350px"} textAlign={"center"}>
+				<EmptySpace variant="DEFAULT" decription="Opa, ainda estamos desenvolvendo uma dashboard para você. Aguarde."></EmptySpace>
+			</Box>
+		</Flex>
+	);
 }
