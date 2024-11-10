d187bd17e7f6cc8cdba00f1347d526d09d542183
Rene Felix <rennebf@gmail.com>
2024-11-09 21:51:19 -0300

    Feat: Ajustes na API

--- public/api/src/auth/reset-password/verify-password.ts
+++ public/api/src/auth/reset-password/verify-password.ts
@@ -1,11 +1,12 @@
 import { Request, Response, Router } from "express";
 import { database } from "../../prisma/client";
 import { errorHandler } from "../../../utils/errorsHandlers";
+import moment from "moment";
 
 const verifyPasswordRouter = Router();
 
 verifyPasswordRouter.get(`/password/verify-reset`, async(req: Request, res: Response) => {
-	const { uuid } = req.query;
+	const { uuid, email } = req.query;
 
 	if (!uuid){
 		res.status(400).send(errorHandler(3, "Preencha todos os campos"));
@@ -13,7 +14,14 @@ verifyPasswordRouter.get(`/password/verify-reset`, async(req: Request, res: Resp
 
 	const isValidReset = await database.reset_password.findFirst({
 		where: {
-			idReset: {equals: `${uuid}`}
+			idReset: {equals: `${uuid}`},
+			users: {
+				emailUser: `${email}`
+			},
+			finishedReset: false,
+			dateExpirationReset: {
+				gte: moment().unix()
+			}
 		}
 	});
 
