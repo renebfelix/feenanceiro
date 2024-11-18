93a0c4704d7fdf1f9e56ca9a1e089dae39d50949
Rene Felix <rennebf@gmail.com>
2024-11-18 01:19:58 -0300

    Suspense share

--- public/web/src/app/(public)/share/layout.tsx
+++ public/web/src/app/(public)/share/layout.tsx
@@ -2,22 +2,24 @@
 
 import { Box, Container } from "@chakra-ui/react";
 import Image from "next/image";
-import React from "react";
+import React, { Suspense } from "react";
 import Logo from '../../../../public/logo-primary-500.svg';
 import Link from "next/link";
 
 export default function ShareLayout({children}: {children: React.ReactNode}){
 	return (
-		<Box minH="100vh" bg={'neutral.200'} py={7}>
-			<Container maxW={"1080px"}>
-				<Box mx={"auto"} maxW={"450px"} mb={"40px"}>
-					<Link href={'/login'}>
-						<Image alt="" src={Logo} />
-					</Link>
-				</Box>
+		<Suspense>
+			<Box minH="100vh" bg={'neutral.200'} py={7}>
+				<Container maxW={"1080px"}>
+					<Box mx={"auto"} maxW={"450px"} mb={"40px"}>
+						<Link href={'/login'}>
+							<Image alt="" src={Logo} />
+						</Link>
+					</Box>
 
-				{children}
-			</Container>
-		</Box>
+					{children}
+				</Container>
+			</Box>
+		</Suspense>
 	)
 }
